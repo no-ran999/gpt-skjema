572fec9e84fe76da244e749488417c35e0539c56
Noran Baskaran <Noran.Baskaran@skatteetaten.no>
2023-11-28 16:25:27 +0100

    co-author: chatgpt, copilot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
+  const [formData, setFormData] = useState({
+    // Initialize your form fields here, e.g., name: '',
+    fieldName: ''
+  });
 
-    </div>
+  const handleChange = (event: { target: { name: any; value: any; }; }) => {
+    const { name, value } = event.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    // Handle form submission, e.g., console.log(formData);
+    console.log(formData);
+  };
+
+  return (
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          {/* Form fields go here */}
+          <input
+              type="text"
+              name="fieldName"
+              value={formData.fieldName}
+              onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
   );
 }
 
